3a56643378c3d6c2854eca746769dac7b2b41e90
Vimala Rajkumar <160380604+Vimala2020@users.noreply.github.com>
2024-03-16 12:28:40 +0530

    modified

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ var signup = document.querySelector(".signup")
 document.getElementById("signup_cross").addEventListener("click",function(){
     signup.style.display="none"
 })
+ var imgslider =document.getElementById("img-slider");
+ function handleClick(direction){
+    imgslider.scrollLeft=direction == 'right'? imgslider.scrollLeft + 300 : imgslider.scrollLeft - 100;
+ }          
 
-
-var sideNavMenu=document.querySelector("title_menubar")
-var sidenavbar = document.querySelector(".side-navbar")
-sideNavMenu.addEventListener("click",function(){
-    sidenavbar.style.marginLeft="0px"
-})
-
-document.getElementById("side-navbar-close").addEventListener("click",()=>{
-    document.querySelector(".side-navbar").style.marginLeft = "-60%"
-})
 
 
